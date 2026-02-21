a13c8425316de1fb623685a73aecb3a3e3456981
Hanumamt Dombale <hanumantdombale60@gmail.com>
2026-02-21 23:06:17 +0530

    refactor: Add some more validation on husky

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,14 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
   },
 ])
